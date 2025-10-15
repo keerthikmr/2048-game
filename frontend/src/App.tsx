d2b13e3932cf8076c6e8c31d46c5cd1bf371ee37
keerthikmr <keerthikumar.m10@gmail.com>
2025-10-16 01:10:30 +0530

    Add padding for board

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Game from "./components/game";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen px-5 bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">2048 Game</h1>
       <Game />
     </div>
