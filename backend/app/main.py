cce83fd41b291a7ec9aeccfb1e1b39d123edd9de
keerthikmr <keerthikumar.m10@gmail.com>
2025-10-17 15:38:32 +0530

    Define root url

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,10 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(router)
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+@app.get("/")
+async def root():
+    return {"message": "Backend running!"}
