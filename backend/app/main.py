61d915ea46a26724824cfc2e89b8b9e8d038dbf8
keerthikmr <keerthikumar.m10@gmail.com>
2025-10-17 15:46:33 +0530

    Add CORS policy, get url from env

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 app.include_router(router)
+
+allowed_origin = os.getenv("FRONTEND_URL", "http://localhost:3000")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[allowed_origin],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
